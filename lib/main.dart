993ca34e9c705b9b21f1661a73de895a213d78c9
pushui <846877359@qq.com>
2019-07-03 00:20:15 +0800

    material-分类

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
-import 'model/post.dart';
- 
+import './model/post.dart';
+import './demo/listView_demo.dart';
+import './demo/hello_demo.dart';
+
 // void main(){
 //   runApp(App());
 // }
@@ -12,6 +14,7 @@ class App extends StatelessWidget{
   Widget build(BuildContext context) {
     
     return MaterialApp(
+      debugShowCheckedModeBanner: false, //去掉右上角debug图标
       home: Home(),
       theme: ThemeData( //设置主题色
         primarySwatch: Colors.red
@@ -22,29 +25,7 @@ class App extends StatelessWidget{
 
 class Home extends StatelessWidget{
 
-//设置每个单元格内的内容 类似于cell
-  Widget _listItemBuilder(BuildContext context,int index){
-    // return Text(posts[index].title);
-    return Container(
-      color: Colors.white,
-      margin: EdgeInsets.all(8.0),
-      child: Column(
-        children: <Widget>[
-          Image.network(posts[index].imageUrl),
-          SizedBox(height: 16.0),
-          Text(
-            posts[index].title,
-            style: Theme.of(context).textTheme.title
-          ),
-          Text(
-            posts[index].author,
-            style: Theme.of(context).textTheme.subhead,
-          ),
-          SizedBox(height: 16.0),
-        ],
-      ),
-    );
-  }
+
   @override
   Widget build(BuildContext context) {
     
@@ -54,27 +35,8 @@ class Home extends StatelessWidget{
           title: Text('朴水'),
           elevation: 1.0,
         ),
-        body: ListView.builder(
-          itemCount: posts.length,
-          itemBuilder: _listItemBuilder,
-        )
+        body: null,//ListViewDemo(),
       );
   }
 }
-class Hello extends StatelessWidget{
-  @override
-  Widget build(BuildContext context) {
-    
-    return Center(
-     child: Text(
-       'hello',
-        textDirection:TextDirection.ltr,
-        style: TextStyle(
-          fontSize: 40.0,
-          fontWeight: FontWeight.bold,
-          color:Colors.purple,
-        )
-     ),
-   );
-  }
-}
+
