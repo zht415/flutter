e68b6dc133e820f231a60e5dfe8d83312e82bc8c
pushui <846877359@qq.com>
2019-07-02 22:30:08 +0800

    界面主题

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,25 @@ class App extends StatelessWidget{
  @override
   Widget build(BuildContext context) {
     // TODO: implement build
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('朴水'),
+          elevation: 1.0,
+        ),
+        body: Hello(),
+      ),
+      theme: ThemeData( //设置主题色
+        primarySwatch: Colors.red
+      ),
+    );
+  }
+}
+
+class Hello extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
     return Center(
      child: Text(
        'hello',
@@ -17,7 +36,7 @@ class App extends StatelessWidget{
         style: TextStyle(
           fontSize: 40.0,
           fontWeight: FontWeight.bold,
-          color:Colors.yellow,
+          color:Colors.purple,
         )
      ),
    );
