fa433ce7ebf40a5f5e3f2eb452e9e432f8e4acbc
pushui <846877359@qq.com>
2019-07-02 23:45:19 +0800

    列表项目

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-
+import 'model/post.dart';
+ 
 // void main(){
 //   runApp(App());
 // }
@@ -9,15 +10,9 @@ void main()=> runApp(App());
 class App extends StatelessWidget{
  @override
   Widget build(BuildContext context) {
-    // TODO: implement build
+    
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('朴水'),
-          elevation: 1.0,
-        ),
-        body: Hello(),
-      ),
+      home: Home(),
       theme: ThemeData( //设置主题色
         primarySwatch: Colors.red
       ),
@@ -25,10 +20,50 @@ class App extends StatelessWidget{
   }
 }
 
+class Home extends StatelessWidget{
+
+  Widget _listItemBuilder(BuildContext context,int index){
+    // return Text(posts[index].title);
+    return Container(
+      color: Colors.white,
+      margin: EdgeInsets.all(8.0),
+      child: Column(
+        children: <Widget>[
+          Image.network(posts[index].imageUrl),
+          SizedBox(height: 16.0),
+          Text(
+            posts[index].title,
+            style: Theme.of(context).textTheme.title
+          ),
+          Text(
+            posts[index].author,
+            style: Theme.of(context).textTheme.subhead,
+          ),
+          SizedBox(height: 16.0),
+        ],
+      ),
+    );
+  }
+  @override
+  Widget build(BuildContext context) {
+    
+    return Scaffold(
+        backgroundColor: Colors.grey[100],
+        appBar: AppBar(
+          title: Text('朴水'),
+          elevation: 1.0,
+        ),
+        body: ListView.builder(
+          itemCount: posts.length,
+          itemBuilder: _listItemBuilder,
+        )
+      );
+  }
+}
 class Hello extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
+    
     return Center(
      child: Text(
        'hello',
