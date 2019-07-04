eddad1e9e1d24b82459a0c50e92cdf4622ae8df0
pushui <846877359@qq.com>
2019-07-04 23:01:59 +0800

    11 GridView.builder 按需生成视图项目_

--- lib/demo/view_demo.dart
+++ lib/demo/view_demo.dart
@@ -5,8 +5,33 @@ import '../model/post.dart';
 class ViewDemo extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    return GridViewExtentDemo();
+    return GridViewBuilderDemo();
   }  
+}
+class GridViewBuilderDemo extends StatelessWidget{
+  
+  Widget _gridItemBuilder(BuildContext context,int index){
+    return Container(
+      child: Image.network(
+        posts[index].imageUrl,
+        fit:BoxFit.cover
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+      padding: EdgeInsets.all(8.0),//添加内边距
+      itemCount: posts.length,
+      itemBuilder: _gridItemBuilder,
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(//用于控制视图的显示,或使用SliverGridDelegateWithMaxCrossAxisExtent
+        crossAxisCount: 4,
+        crossAxisSpacing: 8.0,
+        mainAxisSpacing: 8.0,
+      ),
+    );
+  }
 }
 class GridViewExtentDemo extends StatelessWidget{
   List<Widget>_buildTiles(int length){
@@ -33,7 +58,6 @@ class GridViewExtentDemo extends StatelessWidget{
     );
   }
 }
-
 class GridViewDemo extends StatelessWidget{
   List<Widget>_buildTiles(int length){
     return List.generate(length, (int index){
