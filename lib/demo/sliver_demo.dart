c8e43d374ef0ba0734086f11210d1d3d741132a0
pushui <846877359@qq.com>
2019-07-05 00:06:21 +0800

    6 SliverPadding（内边距） 与 SliverSafeArea

--- lib/demo/sliver_demo.dart
+++ lib/demo/sliver_demo.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/cupertino.dart';
+
 import 'package:flutter/material.dart';
 import '../model/post.dart';
 
@@ -9,12 +9,28 @@ class SliverDemo extends StatelessWidget{
     return Scaffold(
       body:  CustomScrollView(
         slivers: <Widget>[
-          SliverGrid(
+          SliverSafeArea(
+            sliver: SliverPadding(
+            padding: EdgeInsets.all(8.0),
+            sliver: SliverGridDemo(),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class SliverGridDemo extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    
+    return SliverGrid(
             gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
               crossAxisCount: 2,
               crossAxisSpacing: 8.0,
               mainAxisSpacing: 8.0,
-              childAspectRatio: 0.8,//显示的比例
+              childAspectRatio: 1.0,//显示
             ),
             delegate: SliverChildBuilderDelegate(
               (BuildContext context,int index){
@@ -24,11 +40,9 @@ class SliverDemo extends StatelessWidget{
                     fit: BoxFit.cover,  
                   ),
                 );
-              }
+              },
+              childCount: posts.length
             ),
-          )
-        ],
-      ),
-    );
-  }
+          );
+    }
 }
