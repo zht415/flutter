3eb62659ec67b0528929b4daaa51d682af07917c
pushui <846877359@qq.com>
2019-07-04 22:26:04 +0800

    8 PageView.builder 按需生成页

--- lib/demo/view_demo.dart
+++ lib/demo/view_demo.dart
@@ -1,10 +1,50 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import '../model/post.dart';
 
 class ViewDemo extends StatelessWidget{
+  
+  Widget _pageItemBuilder(BuildContext context,int index){
+    return Stack(
+      children: <Widget>[
+        SizedBox.expand(
+          child: Image.network(
+            posts[index].imageUrl,
+            fit: BoxFit.cover,  
+          ),
+        ),
+        Positioned(
+          bottom: 8.0,
+          left: 8.0,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                posts[index].title,
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              Text(
+                posts[index].author
+              ),
+            ],
+          ),
+        )
+      ],
+    );
+  }
   @override
   Widget build(BuildContext context) {
-    
+    return PageView.builder(
+      itemCount: posts.length,
+      itemBuilder: _pageItemBuilder,
+    );
+  }
+}
+
+class PageViewDemo extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
     return PageView(
       // pageSnapping: false,//页面切换到一半时 是否自动弹回 默认为YES
       // reverse: true,//是否倒序显示视图
@@ -13,7 +53,7 @@ class ViewDemo extends StatelessWidget{
       controller: PageController(//管理显示的页面
         initialPage: 1,
         keepPage: false,//是否记住滚动到哪个页面
-        viewportFraction: 0.8,//占页面的比例
+        viewportFraction: 0.5,//占页面的比例
       ),
       children: <Widget>[
         Container(
