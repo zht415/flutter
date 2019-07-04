7035494266cd0ce36dcb388f953bdaebf2c487c8
pushui <846877359@qq.com>
2019-07-04 08:06:33 +0800

    5 Row（横排） 与 Column（竖排

---
+++ lib/demo/layout_demo.dart
@@ -0,0 +1,44 @@
+//布局
+import 'package:flutter/material.dart';
+
+class LayoutDemo extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // return IconBadge(Icons.pool);
+
+    return Container(
+      child: Column(//或设置为Row
+        children: <Widget>[
+          IconBadge(Icons.pool),
+          IconBadge(Icons.beach_access),
+          IconBadge(Icons.airplanemode_active),
+        ],
+      ),
+    );
+    //一个小部件 
+    // return Column(
+    //   children: <Widget>[
+    //     IconBadge(Icons.pool,size: 70),
+    //   ],
+    // );
+  }
+}
+
+class IconBadge extends StatelessWidget{
+  final IconData icon;
+  final double size;
+  
+  IconBadge(this.icon,{
+    this.size = 32.0
+  });
+  @override
+  Widget build(BuildContext context) {
+    
+    return Container(
+      child: Icon(icon,size:size,color: Colors.white),
+      width: size+60,
+      height: size+60,
+      color: Color.fromRGBO(3, 54, 255, 1.0),
+    );
+  }
+}
