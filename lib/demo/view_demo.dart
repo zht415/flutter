5f6ea6776730810d9493c3e2d84d186c1ae9f1e7
pushui <846877359@qq.com>
2019-07-04 21:53:42 +0800

    5 PageView：页面视图

--- lib/demo/view_demo.dart
+++ lib/demo/view_demo.dart
@@ -3,7 +3,34 @@ import 'package:flutter/material.dart';
 class ViewDemo extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return Container();
+    
+    return PageView(
+      children: <Widget>[
+        Container(
+          color: Colors.brown[900],
+          alignment: Alignment(0.0, 0.0),
+          child: Text(
+            'ONE',
+            style: TextStyle(fontSize: 32.0,color: Colors.white),
+          ),
+        ),
+        Container(
+          color: Colors.blue[900],
+          alignment: Alignment(0.0, 0.0),
+          child: Text(
+            'TWO',
+            style: TextStyle(fontSize: 32.0,color: Colors.red),
+          ),
+        ),
+        Container(
+          color: Colors.cyan[900],
+          alignment: Alignment(0.0, 0.0),
+          child: Text(
+            'THREE',
+            style: TextStyle(fontSize: 32.0,color: Colors.green),
+          ),
+        ),
+      ],
+    );
   }
 }
