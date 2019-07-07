f5dfb30d497c8150b27a53c557a1e21981b100e5
pushui <846877359@qq.com>
2019-07-07 10:41:08 +0800

    14 showDatePicker：获取选择的日期（async

--- lib/demo/datetime_demo.dart
+++ lib/demo/datetime_demo.dart
@@ -2,6 +2,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'dart:async';
 
 class DateTimeDemo extends StatefulWidget {
   @override
@@ -10,14 +11,19 @@ class DateTimeDemo extends StatefulWidget {
 
 class _DateTimeState extends State<DateTimeDemo> {
   //选择的时间
-  final DateTime selectedDate = DateTime.now();
-  _selectDate(){
-    showDatePicker(
+  DateTime selectedDate = DateTime.now();
+  //日期选择窗口
+  _selectDate() async{
+    final DateTime date = await showDatePicker(
       context: context,
       initialDate: selectedDate,//初始日期
       firstDate: DateTime(1900),
       lastDate: DateTime(2100),
     );
+    if(date == null) return;
+    setState(() {
+      selectedDate = date;
+    });
   }
   @override
   Widget build(BuildContext context) {
@@ -38,7 +44,8 @@ class _DateTimeState extends State<DateTimeDemo> {
                   onTap: _selectDate,
                   child: Row(
                     children: <Widget>[
-                      Text(DateFormat.yMd().format(selectedDate)),
+                      // Text(DateFormat.yMd().format(selectedDate)),
+                      Text(DateFormat.yMMMMd().format(selectedDate)),
                       Icon(Icons.arrow_drop_down),
                     ],
                   ),
