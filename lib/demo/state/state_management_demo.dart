473d2c652f139224aea9520122159de6a4cc40ff
pushui <846877359@qq.com>
2019-07-07 19:11:49 +0800

    3 创建 StateManagementDemo 小部件_【itjc8.com】

---
+++ lib/demo/state/state_management_demo.dart
@@ -0,0 +1,14 @@
+//状态管理
+
+import 'package:flutter/material.dart';
+class StateManagementDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('StateManagementDemo'),
+        elevation: 0.0,
+      ),
+    );
+  }
+}
