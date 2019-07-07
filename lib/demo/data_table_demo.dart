7a5fd92e6ce582d5d6e818fc13f974a47f40ad78
pushui <846877359@qq.com>
2019-07-07 16:36:41 +0800

    12 DataTable：用列表生成数据表格_【itjc8.com】

--- lib/demo/data_table_demo.dart
+++ lib/demo/data_table_demo.dart
@@ -1,6 +1,7 @@
 //数据表格
 
 import 'package:flutter/material.dart';
+import '../model/post.dart';
 
 class DataTableDemo extends StatefulWidget {
   @override
@@ -22,32 +23,47 @@ class _DataTableDemoState extends State<DataTableDemo> {
            DataTable(
              columns: [
                DataColumn(
-                 label: Text('Title'),
+                 label: Container(
+                   width: 80.0,
+                   child: Text('Title'),
+                 ),
                ),
                DataColumn(
                  label: Text('Author'),
                ),
-             ],
-             rows: [
-               DataRow(
-                 cells: [
-                   DataCell(Text('Hello~')),
-                   DataCell(Text('PuShui')),
-                 ]
-               ),
-                DataRow(
-                 cells: [
-                   DataCell(Text('Hola~')),
-                   DataCell(Text('朴水')),
-                 ]
-               ),
-               DataRow(
-                 cells: [
-                   DataCell(Text('Morning~')),
-                   DataCell(Text('Pata')),
-                 ]
+               DataColumn(
+                 label: Text('Image'),
                ),
              ],
+             rows: posts.map((post){
+               return DataRow(
+                 cells: [
+                   DataCell(Text(post.title)),
+                   DataCell(Text(post.author)),
+                   DataCell(Image.network(post.imageUrl)),
+                 ]
+               );
+             }).toList(),
+            //  rows: [
+            //    DataRow(
+            //      cells: [
+            //        DataCell(Text('Hello~')),
+            //        DataCell(Text('PuShui')),
+            //      ]
+            //    ),
+            //     DataRow(
+            //      cells: [
+            //        DataCell(Text('Hola~')),
+            //        DataCell(Text('朴水')),
+            //      ]
+            //    ),
+            //    DataRow(
+            //      cells: [
+            //        DataCell(Text('Morning~')),
+            //        DataCell(Text('Pata')),
+            //      ]
+            //    ),
+            //  ],
            ),
           ],
         ),
