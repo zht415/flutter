7dafa78f0aa015480e242be4eefd5f3539cdf524
pushui <846877359@qq.com>
2019-07-07 10:24:08 +0800

    11 安装第三方包（演示安装 Intl 包）_【itjc8.com】

---
+++ lib/demo/slider_demo.dart
@@ -0,0 +1,52 @@
+//Slider
+
+import 'package:flutter/material.dart';
+
+class SliderDemo extends StatefulWidget {
+  @override
+  _SliderDemoState createState() => _SliderDemoState();
+}
+
+class _SliderDemoState extends State<SliderDemo> {
+
+  double _sliderItemA = 0.0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('_SliderDemo'),
+        elevation: 0.0,
+      ),
+      body: Container(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Row( 
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Slider(
+                  value: _sliderItemA,
+                  onChanged: (value){
+                    setState(() {
+                      _sliderItemA = value;
+                    });
+                  },
+                  activeColor: Theme.of(context).accentColor,
+                  inactiveColor: Theme.of(context).accentColor.withOpacity(0.3),
+                  min: 0.0,
+                  max: 50.0,
+                  divisions: 5,//分割份数
+                  label: '${_sliderItemA.toInt()}',
+                ),
+              ],
+            ),
+            SizedBox(height: 16.0),
+            Text('SliderValue:$_sliderItemA'),
+          ],
+        ),
+      ),
+    );
+  }
+}
