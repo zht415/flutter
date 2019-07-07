6a592815bc00dfddb37d6ed63be2201af4d5a3ea
pushui <846877359@qq.com>
2019-07-07 15:07:43 +0800

    12 ExpansionPanel：面板列表项目

--- lib/demo/expansion_panel_demo.dart
+++ lib/demo/expansion_panel_demo.dart
@@ -1,5 +1,15 @@
 import 'package:flutter/material.dart';
 
+class ExpansionPanelItem{
+  final String headerText;
+  final Widget body;
+  bool isExpanded;
+  ExpansionPanelItem({
+    this.headerText,
+    this.body,
+    this.isExpanded,
+  });
+}
 class ExpansionPanelDemo extends StatefulWidget {
   @override
   _ExpansionPanelDemoState createState() => _ExpansionPanelDemoState();
@@ -7,12 +17,48 @@ class ExpansionPanelDemo extends StatefulWidget {
 
 class _ExpansionPanelDemoState extends State<ExpansionPanelDemo> {
 
-  bool _isExpanded = false;
+  // bool _isExpanded = false;
+  List<ExpansionPanelItem> _expansionPanelItems;
+  
+ @override
+  void initState() { //初始化
+    super.initState();
+    _expansionPanelItems = <ExpansionPanelItem>[
+      ExpansionPanelItem(
+        headerText: 'Panel A',
+        body: Container(
+          padding: EdgeInsets.all(16.0),
+          width: double.infinity,
+          child: Text('Content for Panel A.'),
+        ),
+        isExpanded: false,
+      ),
+      ExpansionPanelItem(
+        headerText: 'Panel B',
+        body: Container(
+          padding: EdgeInsets.all(16.0),
+          width: double.infinity,
+          child: Text('Content for Panel B.'),
+        ),
+        isExpanded: false,
+      ),
+      ExpansionPanelItem(
+        headerText: 'Panel C',
+        body: Container(
+          padding: EdgeInsets.all(16.0),
+          width: double.infinity,
+          child: Text('Content for Panel C .'),
+        ),
+        isExpanded: false,
+      ),
+
+    ];
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('_WidgetDemo'),
+        title: Text('ExpansionPanelDemo'),
         elevation: 0.0,
       ),
       body: Container(
@@ -23,28 +69,47 @@ class _ExpansionPanelDemoState extends State<ExpansionPanelDemo> {
             ExpansionPanelList(
               expansionCallback:(int panelIndex,bool isExpanded){
                 setState(() {
-                  _isExpanded = !isExpanded; 
+                  // _isExpanded = !isExpanded; 
+                  _expansionPanelItems[panelIndex].isExpanded = !isExpanded;
                 });
               },
-              children: [
-                ExpansionPanel(
-                  headerBuilder: (BuildContext context,bool isExpanded){
-                    return Container(
-                      padding: EdgeInsets.all(16.0),
-                      child: Text(
-                        'Panel A',
-                        style:Theme.of(context).textTheme.title,
-                      ),
-                    );
-                  },
-                  body: Container(
-                    padding: EdgeInsets.all(16.0),
-                    width: double.infinity,
-                    child: Text('Content for Panel A.'),
-                  ),
-                  isExpanded: _isExpanded,//展示还是收起
-                ),
-              ],
+              children: _expansionPanelItems.map( //通过map获取每个Item
+                (ExpansionPanelItem item){
+                  return ExpansionPanel(
+                    isExpanded:  item.isExpanded,
+                    body: item.body,
+                    headerBuilder: (BuildContext context,bool isExpanded){
+                      return Container(
+                        padding: EdgeInsets.all(16.0),
+                        child: Text(
+                          // 'Panel A',
+                          item.headerText,
+                          style:Theme.of(context).textTheme.title,
+                        ),
+                      );
+                    },
+                  );
+                }
+              ).toList(),
+              // children: [
+              //   ExpansionPanel(
+              //     headerBuilder: (BuildContext context,bool isExpanded){
+              //       return Container(
+              //         padding: EdgeInsets.all(16.0),
+              //         child: Text(
+              //           'Panel A',
+              //           style:Theme.of(context).textTheme.title,
+              //         ),
+              //       );
+              //     },
+              //     body: Container(
+              //       padding: EdgeInsets.all(16.0),
+              //       width: double.infinity,
+              //       child: Text('Content for Panel A.'),
+              //     ),
+              //     isExpanded: _isExpanded,//展示还是收起
+              //   ),
+              // ],
             ),
           ],
         ),
