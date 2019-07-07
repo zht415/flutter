94ce5d1d83b37f6aa4e7fd5d4360dc510717fff3
pushui <846877359@qq.com>
2019-07-07 18:54:52 +0800

    19 Stepper：步骤_【itjc8.com】

--- lib/demo/material_component.dart
+++ lib/demo/material_component.dart
@@ -18,6 +18,7 @@ import './expansion_panel_demo.dart';
 import './chip_demo.dart';
 import './paginated_data_table_demo.dart';
 import './card_demo.dart';
+import './stepper_demo.dart';
 
 class MaterialComponents extends StatelessWidget {
   @override
@@ -29,6 +30,7 @@ class MaterialComponents extends StatelessWidget {
       ),
       body: ListView(
         children: <Widget>[
+          ListItem(title: 'Stepper',page: StepperDemo()),          
           ListItem(title: 'Card',page: CardDemo()),          
           ListItem(title: 'PaginatedDataTable',page: PaginatedDataTableDemo()),          
           ListItem(title: 'DataTable',page: DataTableDemo()),          
