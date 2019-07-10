1781b3d4897bac28212a9775923fdf069a3e4249
pushui <846877359@qq.com>
2019-07-10 08:23:58 +0800

    6 使用 InheritedWidget 传递 BLoC_【itjc8.com】

--- lib/demo/bloc/bloc_demo.dart
+++ lib/demo/bloc/bloc_demo.dart
@@ -1,14 +1,21 @@
+//业务逻辑组件
+
 import 'package:flutter/material.dart';
+import 'package:flutter_pushui/demo/bloc/counter_bloc_demo.dart';
 
 class BlocDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('BlocDemo'),
-        elevation: 0.0,
+    return CounterProvider(
+      bloc: CounterBloc(),
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('BlocDemo'),
+          elevation: 0.0,
+        ),
+        body: CounterHome(),
+        floatingActionButton: CounterActionButton(),
       ),
     );
   }
 }
-
