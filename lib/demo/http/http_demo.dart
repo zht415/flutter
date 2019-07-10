f716de9ba15045c03db33a2ad282ed65767f5dfc
pushui <846877359@qq.com>
2019-07-10 09:04:25 +0800

    4 安装 http 包（升级 Flutter SDK）_【itjc8.com】

---
+++ lib/demo/http/http_demo.dart
@@ -0,0 +1,14 @@
+
+import 'package:flutter/material.dart';
+
+class HttpDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('HttpDemo'),
+        elevation: 0.0,
+      ),
+    );
+  }
+}
