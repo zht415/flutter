c5021dc469e0f4d4bac9c2238d7d05a19f59b7df
pushui <846877359@qq.com>
2019-07-10 21:40:27 +0800

    5 使用 http 客户端请求数据_【itjc8.com】

--- lib/demo/http/http_demo.dart
+++ lib/demo/http/http_demo.dart
@@ -1,5 +1,7 @@
 
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:async';
 
 class HttpDemo extends StatelessWidget {
   @override
@@ -9,6 +11,33 @@ class HttpDemo extends StatelessWidget {
         title: Text('HttpDemo'),
         elevation: 0.0,
       ),
+      body: HttpDemoHome(),
+    );
+  }
+}
+
+class HttpDemoHome extends StatefulWidget {
+  @override
+  _HttpDemoHomeState createState() => _HttpDemoHomeState();
+}
+
+class _HttpDemoHomeState extends State<HttpDemoHome> {
+  
+  @override
+  void initState() {
+    fetchPost();
+  }
+  fetchPost() async{
+    final response = 
+        await http.get('https://resources.ninghao.net/demo/posts.json');
+    print('statusCode:${response.statusCode}');
+    print('body:${response.body}');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
     );
   }
 }
